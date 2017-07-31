c51f55b608805f9128fff3e98d2abd30413b5e83
hishamhwr <hisham.hawara@mail.mcgill.ca>
2017-07-31 11:54:16 -0400

    update 2

--- app.py
+++ app.py
@@ -78,37 +78,33 @@ def get_historical(quote):
 
 def stock_prediction():
 
-	    dataset = []
+    dataset = []
 
-	    with open(FILE_NAME) as f:
-	        for n, line in enumerate(f):
-	            if n != 0:
-	                dataset.append(float(line.split(',')[1]))
+    with open(FILE_NAME) as f:
+        for n, line in enumerate(f):
+            if n != 0:
+                dataset.append(float(line.split(',')[1]))
 
-	    dataset = np.array(dataset)
+    dataset = np.array(dataset)
 
-	    # Create dataset matrix (X=t and Y=t+1)
-	    def create_dataset(dataset):
-	        dataX = [dataset[n+1] for n in range(len(dataset)-2)]
-	        return np.array(dataX), dataset[2:]
-	        
-	    trainX, trainY = create_dataset(dataset)
+    # Create dataset matrix (X=t and Y=t+1)
+    def create_dataset(dataset):
+        dataX = [dataset[n+1] for n in range(len(dataset)-2)]
+        return np.array(dataX), dataset[2:]
+        
+    trainX, trainY = create_dataset(dataset)
 
-	    # Create and fit Multilinear Perceptron model
-	    model = Sequential()
-	    model.add(Dense(8, input_dim=1, activation='relu'))
-	    model.add(Dense(1))
-	    model.compile(loss='mean_squared_error', optimizer='adam')
-	    model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
+    # Create and fit Multilinear Perceptron model
+    model = Sequential()
+    model.add(Dense(8, input_dim=1, activation='relu'))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
 
-	    prediction = model.predict(np.array([dataset[0]]))
-	    if ((prediction[0][0]-dataset[0]) > 0):
-	    	result = 'The price will go up from %s to %s' % (dataset[0], prediction[0][0])
-	    else:
-	    	result = 'The price will drop from %s to %s' % (dataset[0], prediction[0][0])
-	    
+    prediction = model.predict(np.array([dataset[0]]))
+    result = 'The price will move from %s to %s' % (dataset[0], prediction[0][0])
 
-	    return result
+    return result
 
     
 # Ask user for a stock quote
